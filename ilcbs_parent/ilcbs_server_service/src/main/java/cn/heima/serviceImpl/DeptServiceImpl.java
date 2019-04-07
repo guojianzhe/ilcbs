9266bc5275c5532bf8816cd45fad4da47c85cebe
guojianzhe <874375365@qq.com>
2019-04-07 23:51:52 +0800

    4.7.23

---
+++ ilcbs_parent/ilcbs_server_service/src/main/java/cn/heima/serviceImpl/DeptServiceImpl.java
@@ -0,0 +1,65 @@
+package cn.heima.serviceImpl;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import cn.heima.dao.DeptDao;
+import cn.heima.domain.Dept;
+import cn.heima.service.DeptService;
+@Service("deptService")
+public class DeptServiceImpl implements DeptService{
+
+	@Autowired
+	private DeptDao deptDao;
+	
+	@Override
+	public List<Dept> find(Specification<Dept> spec) {
+		// TODO Auto-generated method stub
+		return deptDao.findAll(spec);
+	}
+
+	@Override
+	public Dept get(String id) {
+		
+		return deptDao.findOne(id);
+	}
+
+	@Override
+	public Page<Dept> findPage(Specification<Dept> spec, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return deptDao.findAll(spec, pageable);
+	}
+
+	@Override
+	public void saveOrUpdate(Dept entity) {
+		deptDao.save(entity);
+		
+	}
+
+	@Override
+	public void saveOrUpdateAll(Collection<Dept> entitys) {
+		deptDao.save(entitys);
+		
+	}
+
+	@Override
+	public void deleteById(String id) {
+		deptDao.delete(id);
+		
+	}
+
+	@Override
+	public void delete(String[] ids) {
+		for (String id : ids) {
+			deptDao.delete(id);
+		}
+		
+	}
+
+}
--- ilcbs_parent/ilcbs_server_web/src/main/java/cn/heima/web/action/sysadmin/DeptAction.java
+++ ilcbs_parent/ilcbs_server_web/src/main/java/cn/heima/web/action/sysadmin/DeptAction.java
@@ -1,5 +1,11 @@
 package cn.heima.web.action.sysadmin;
 
+import java.util.List;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.itextpdf.text.pdf.PdfStructTreeController.returnType;
 import com.opensymphony.xwork2.ModelDriven;
 
 import cn.heima.domain.Dept;
@@ -8,10 +14,20 @@ import cn.heima.web.action.BaseAction;
 public class DeptAction extends BaseAction implements ModelDriven<Dept>
 {
 
+	private static final long serialVersionUID = -2378891858335431965L;
+	
+	private Dept model = new Dept();
+	
 	@Override
 	public Dept getModel() {
 		
-		return null;
+		return model;
+	}
+	@Action(value="deptAction",results= {@Result(name="list",location="/WEB-INF/pages/sysadmin/dept/jDeptList.jsp")})
+	public String list() {
+		
+		
+		return "list";
 	}
 
 }
